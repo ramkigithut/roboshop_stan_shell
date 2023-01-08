ece096b9fcb59f718ec0c8231af9c7b6b11034d6
ramkigithut <reddithi.krishna@gmail.com>
2023-01-08 21:03:05 +0530

    mongodb repo file

---
+++ catalogue.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+conf_file_location=$(pwd)
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+yum install nodejs -y
+useradd
+mkdir /app
+curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
+cd /app
+unzip /tmp/catalogue.
+cd /app
+npm install
+
+cp ${conf_file_location}/files/catalogue.service /etc/systemd/system/catalogue.service
+systemctl daemon-reload
+systemctl enable catalogue
+systemctl start catalogue
