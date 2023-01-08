0a20f64139319cddd1bd715c0901d48a8d045617
ramkigithut <reddithi.krishna@gmail.com>
2023-01-08 20:16:07 +0530

    roboshop frontend script

---
+++ mongodb.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env
+conf_fie_location = $(pwd)
+cp ${conf_fie_location}/files/mongodb.repo /etc/yum.repos.d/mongodb.repo
+
+yum install mongodb-org -y
+systemctl enable mongod
+systemctl start mongod
