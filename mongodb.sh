ed2a811b9d990c4d0a132e52dfae820975877d84
ramkigithut <reddithi.krishna@gmail.com>
2023-01-08 20:48:44 +0530

    mongodb repo file

--- mongodb.sh
+++ mongodb.sh
@@ -3,5 +3,8 @@ conf_fie_location=$(pwd)
 cp ${conf_fie_location}/files/mongodb.repo /etc/yum.repos.d/mongodb.repo
 
 yum install mongodb-org -y
+
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+
 systemctl enable mongod
 systemctl start mongod
