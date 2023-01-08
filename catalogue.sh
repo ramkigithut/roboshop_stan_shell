a243819d487fcde637e002ee5d1f8b2546386648
ramkigithut <reddithi.krishna@gmail.com>
2023-01-08 21:25:30 +0530

    catalogue.service

--- catalogue.sh
+++ catalogue.sh
@@ -3,8 +3,8 @@ conf_file_location=$(pwd)
 set -e
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 yum install nodejs -y
-useradd roboshop
-mkdir /app
+#useradd roboshop
+mkdir -p /app
 curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
 cd /app
 unzip /tmp/catalogue.
