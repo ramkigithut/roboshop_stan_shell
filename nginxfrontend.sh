b770f8bee6291e390103f65a3a89404ef4a379be
ramkigithut <reddithi.krishna@gmail.com>
2023-01-10 00:27:32 +0530

    creation of DNS Records type A

--- nginxfrontend.sh
+++ nginxfrontend.sh
@@ -1,24 +1,26 @@
 #!/usr/bin/env bash
 conf_file_location=$(pwd)
+log=/tmp/roboshop.log
+
 echo -e "\e[35m Install nginx\e[0m"
-yum install nginx -y
+yum install nginx -y &>>${log}
 
 echo -e "\e[35m Remove nginx default content\e[0m"
-rm -rf /usr/share/nginx/html/*
+rm -rf /usr/share/nginx/html/* &>>${log}
 
 echo -e "\e[35m Download Frontend Content\e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
-cd /usr/share/nginx/html
+cd /usr/share/nginx/html &>>${log}
 
 echo -e "\e[35m Extract Frontend Content\e[0m"
-unzip /tmp/frontend.zip
+unzip /tmp/frontend.zip &>>${log}
 
 echo -e "\e[35m Copy Roboshop Nginx Config file\e[0m"
 cp ${conf_file_location}/files/nginx-server-roboshop.conf /etc/nginx/default.d/roboshop.conf
 
 echo -e "\e[35m Enable Nginx\e[0m"
-systemctl enable nginx
+systemctl enable nginx &>>${log}
 
 echo -e "\e[35m Start Nginx\e[0m"
-systemctl start nginx
+systemctl start nginx &>>${log}
 
