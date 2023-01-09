3c1cab3df7d7cf605b76ec2d8b54e2f2115787e3
ramkigithut <reddithi.krishna@gmail.com>
2023-01-10 05:13:23 +0530

    status_check function added to print status after execution

--- catalogue.sh
+++ catalogue.sh
@@ -4,7 +4,7 @@ log=/tmp/roboshop.log
 
 status_check() {
   if [ $? -eq 0 ]; then
-    echo success
+    echo -e "\e[32msuccess\e[0m"
   else
     echo -e "\e[31mfailure\e[0m"
     echo "Refer log file for more information, log is at ${log}"
