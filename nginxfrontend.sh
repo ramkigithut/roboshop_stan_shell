304b5d351d839dbc4081182db21ada74b5432d15
ramkigithut <reddithi.krishna@gmail.com>
2023-01-10 16:28:51 +0530

    print_head function added and different arguments are passed on at each stage in the script

--- nginxfrontend.sh
+++ nginxfrontend.sh
@@ -1,36 +1,36 @@
 source common.sh
 
-print_head "Install nginx\e[0m"
+print_head "Install nginx"
 yum install nginx -y &>>${log}
 status_check
 
 
-print_head "Remove nginx default content\e[0m"
+print_head "Remove nginx default content"
 rm -rf /usr/share/nginx/html/* &>>${log}
 status_check
 
-print_head "Download Frontend Content\e[0m"
+print_head "Download Frontend Content"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${log}
 status_check
 
 cd /usr/share/nginx/html &>>${log}
 
 
-print_head "Extract Frontend Content\e[0m"
+print_head "Extract Frontend Content"
 unzip /tmp/frontend.zip &>>${log}
 status_check
 
 
-print_head "Copy Roboshop Nginx Config file\e[0m"
+print_head "Copy Roboshop nginx Config file"
 cp ${conf_file_location}/files/nginx-server-roboshop.conf /etc/nginx/default.d/roboshop.conf
 status_check
 
 
-print_head "Enable Nginx\e[0m"
+print_head "Enable nginx"
 systemctl enable nginx &>>${log}
 status_check
 
-print_head "Start Nginx\e[0m"
+print_head "Start nginx"
 systemctl start nginx &>>${log}
 status_check
 
