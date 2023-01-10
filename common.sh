64aa4351e99f83f9d37e547c4655f0dc82c24c03
ramkigithut <reddithi.krishna@gmail.com>
2023-01-10 06:03:55 +0530

    common.sh called to address the intended function of status check globally

--- common.sh
+++ common.sh
@@ -4,9 +4,9 @@ log=/tmp/roboshop.log
 
 status_check() {
   if [ $? -eq 0 ]; then
-    echo -e "\e[32msuccess\e[0m"
+    echo -e "\e[1;32msuccess\e[0m"
   else
-    echo -e "\e[31mfailure\e[0m"
+    echo -e "\e[1;31mfailure\e[0m"
     echo "Refer log file for more information, log is at ${log}"
     exit
   fi
