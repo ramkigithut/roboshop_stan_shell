b41085a22725e9f99e98703b927d284e6c763617
ramkigithut <reddithi.krishna@gmail.com>
2023-01-10 16:31:35 +0530

    print_head function added and different arguments are passed on at each stage in the script

--- catalogue.sh
+++ catalogue.sh
@@ -1,63 +1,63 @@
 source common.sh
 
-echo -e "\e[35m Configuring nodejs repos\e[0m"
+print_head "Configuring nodejs repos"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log}
 status_check
 
-echo -e "\e[35m Install nodejs\e[0m"
+print_head "Install nodejs"
 yum install nodejs -y &>>${log}
 status_check
 
-echo -e "\e[35m Add Application user\e[0m"
+print_head "Add Application user"
 useradd roboshop &>>${log}
 status_check
 
 mkdir -p /app &>>${log}
 
-echo -e "\e[35m Downloading App Content\e[0m"
+print_head "Downloading App Content"
 curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>${log}
 status_check
 
-echo -e "\e[35m Cleanup Old Content\e[0m"
+print_head "Cleanup Old Content"
 rm -rf /app/* &>>${log}
 status_check
 
-echo -e "\e[35m CleaExtracting App Content\e[0m"
+print_head "CleaExtracting App Content"
 cd /app &>>${log}
 unzip /tmp/catalogue.zip &>>${log}
 status_check
 
-echo -e "\e[35m Installing nodejs Dependencies\e[0m"
+print_head "Installing nodejs Dependencies"
 cd /app &>>${log}
 npm install -g npm@9.2.0 &>>${log}
 status_check
 
-echo -e "\e[35m Configuring catalogue service file\e[0m"
+print_head "Configuring catalogue service file"
 cp ${conf_file_location}/files/catalogue.service /etc/systemd/system/catalogue.service &>>${log}
 status_check
 
-echo -e "\e[35m Reload SystemD\e[0m"
+print_head "Reload SystemD"
 systemctl daemon-reload &>>${log}
 status_check
 
 
-echo -e "\e[35m Enable catalogue service\e[0m"
+print_head "Enable catalogue service"
 systemctl enable catalogue &>>${log}
 status_check
 
 
-echo -e "\e[35m start catalogue service\e[0m"
+print_head "start catalogue service"
 systemctl start catalogue &>>${log}
 status_check
 
-echo -e "\e[35m Configuring mongo repos\e[0m"
+print_head "Configuring mongo repos"
 cp ${conf_file_location}/files/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>${log}
 status_check
 
-echo -e "\e[35m Installing mongo client\e[0m"
+print_head "Installing mongo client"
 yum install mongodb-org-shell -y &>>${log}
 status_check
 
-echo -e "\e[35m load schema\e[0m"
+print_head "load schema"
 mongo --host mongodb-dev.practicaldevops.online </app/schema/catalogue.js &>>${log}
 status_check
