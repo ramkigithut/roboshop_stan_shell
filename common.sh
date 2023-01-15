0d55550b1cb4dfc5866ea723190e69290a66b76f
ramkigithut <reddithi.krishna@gmail.com>
2023-01-16 03:47:52 +0530

    maven function added to common.sh script

--- common.sh
+++ common.sh
@@ -16,78 +16,82 @@ print_head() {
   echo -e "\e[1; $1 \e[0m"
   }
 
-  NODEJS() {
-    print_head "Configuring nodejs repos"
-    curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log}
+NODEJS() {
+
+  print_head "Configuring nodejs repos"
+  curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log}
+  status_check
+
+  print_head "Install nodejs"
+  yum install nodejs -y &>>${log}
+  status_check
+
+  print_head "Add Application user"
+  id roboshop &>>${log}
+  if [ $? -ne 0 ]; then
+    useradd roboshop &>>${log}
+  fi
+  status_check
+
+  mkdir -p /app &>>${log}
+
+  print_head "Downloading App Content"
+  curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log}
+  status_check
+
+  print_head "Cleanup Old Content"
+  rm -rf /app/* &>>${log}
+  status_check
+
+  print_head "CleaExtracting App Content"
+  cd /app &>>${log}
+  unzip /tmp/${component}.zip &>>${log}
+  status_check
+
+  print_head "Installing nodejs Dependencies"
+  cd /app &>>${log}
+  npm install &>>${log}
+  status_check
+
+  print_head "Configuring catalogue service file"
+  cp ${conf_file_location}/files/${component}.service /etc/systemd/system/${component}.service &>>${log}
+  status_check
+
+  print_head "Reload SystemD"
+  systemctl daemon-reload &>>${log}
+  status_check
+
+
+  print_head "Enable ${component} service"
+  systemctl enable ${component} &>>${log}
+  status_check
+
+
+  print_head "start ${component} service"
+  systemctl start ${component} &>>${log}
+  status_check
+
+
+  if [ ${schema_load} == "true" ]; then
+
+    print_head "Configuring mongo repos"
+    cp ${conf_file_location}/files/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>${log}
     status_check
 
-    print_head "Install nodejs"
-    yum install nodejs -y &>>${log}
+    print_head "Installing mongo client"
+    yum install mongodb-org-shell -y &>>${log}
     status_check
 
-    print_head "Add Application user"
-    id roboshop &>>${log}
-    if [ $? -ne 0 ]; then
-      useradd roboshop &>>${log}
-    fi
+    print_head "load schema"
+    mongo --host mongodb-dev.practicaldevops.online </app/schema/${component}.js &>>${log}
     status_check
+  fi
+}
 
-    mkdir -p /app &>>${log}
+MAVEN(){
 
-    print_head "Downloading App Content"
-    curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log}
-    status_check
+  print_head "Install Maven"
+  yum install maven -y &>>${log}
+  status_check
 
-    print_head "Cleanup Old Content"
-    rm -rf /app/* &>>${log}
-    status_check
-
-    print_head "CleaExtracting App Content"
-    cd /app &>>${log}
-    unzip /tmp/${component}.zip &>>${log}
-    status_check
-
-    print_head "Installing nodejs Dependencies"
-    cd /app &>>${log}
-    npm install &>>${log}
-    status_check
-
-    print_head "Configuring catalogue service file"
-    cp ${conf_file_location}/files/${component}.service /etc/systemd/system/${component}.service &>>${log}
-    status_check
-
-    print_head "Reload SystemD"
-    systemctl daemon-reload &>>${log}
-    status_check
-
-
-    print_head "Enable ${component} service"
-    systemctl enable ${component} &>>${log}
-    status_check
-
-
-    print_head "start ${component} service"
-    systemctl start ${component} &>>${log}
-    status_check
-
-
-    if [ ${schema_load} == "true" ]; then
-
-      print_head "Configuring mongo repos"
-      cp ${conf_file_location}/files/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>${log}
-      status_check
-
-      print_head "Installing mongo client"
-      yum install mongodb-org-shell -y &>>${log}
-      status_check
-
-      print_head "load schema"
-      mongo --host mongodb-dev.practicaldevops.online </app/schema/${component}.js &>>${log}
-      status_check
-    fi
-
-  }
-
-  MAVEN(){
-
-  }
+}
