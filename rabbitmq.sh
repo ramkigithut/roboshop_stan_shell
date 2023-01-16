41f95e64534526e1a4203a32200dff782f9f3143
ramkigithut <reddithi.krishna@gmail.com>
2023-01-16 06:53:43 +0530

    rabbitmq script

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -6,34 +6,34 @@ if [ -z "${roboshop_rabbitmq_password}"]; then
 fi
 
 print_head "Configuring Erlnag YUM Repos"
-curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | sudo
+curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | sudo bash &>>${log}
 status_check
 
 print_head "Configuring RabbitMQ YUM Repos"
-curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash &>>${log}
 status_check
 
 print_head "Install Erlang & RabbitMQ"
-yum install erlang rabbitmq-server -y
+yum install erlang rabbitmq-server -y &>>${log}
 status_check
 
 print_head "Enable RabbitMQ Server"
-systemctl enable rabbitmq-server
+systemctl enable rabbitmq-server &>>${log}
 status_check
 
 print_head "Start RabbitMQ Server"
-systemctl start rabbitmq-server
+systemctl start rabbitmq-server &>>${log}
 status_check
 
 
 print_head "Add application User"
-rabbitmqctl add_user roboshop ${roboshop_rabbitmq_password}
+rabbitmqctl add_user roboshop ${roboshop_rabbitmq_password} &>>${log}
 status_check
 
 print_head "Add Tags to application User "
-rabbitmqctl set_user_tags roboshop administrator
+rabbitmqctl set_user_tags roboshop administrator &>>${log}
 status_check
 
 print_head "Add Permission to application User"
-rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>${log}
 status_check
